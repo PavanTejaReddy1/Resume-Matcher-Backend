5e1af747de9826648e81deda905f51b190d05db4
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2026-03-02 22:26:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { PDFParse } = require("pdf-parse");
 const Groq = require("groq-sdk");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 app.use(express.json({ limit: "10MB" }));
 app.use(cors());
@@ -18,7 +18,7 @@ app.listen(PORT, () => console.log("Server running at", PORT));
 
 app.post("/send", async (req, res) => {
     let { jd, base64 } = req.body;
-    
+
     base64 = base64.split(",")[1];
 
     const buffer = Buffer.from(base64, "base64");
@@ -38,12 +38,26 @@ app.post("/send", async (req, res) => {
             },
             {
                 role: "user",
-                content: `Compare the following Resume and Job Description.
+                content: `Compare the following Resume and Job Description carefully.
                 Resume: ${textFromPdf.text}
                 Job Description: ${jd}
-                Return ONLY a JSON response in this format:
+                
+                Act as a strict ATS resume scoring system.
+                Instructions:
+                - Analyze skill match, experience relevance, and keyword alignment.
+                - Be realistic and slightly strict while scoring.
+                - The score must be between 0 and 100.
+                - The reason must clearly explain why that score was given in 3-5 sentences.- Extract important keywords only.
+                
+                Return ONLY valid JSON.
+                Do NOT include markdown.
+                Do NOT include backticks.
+                Do NOT include extra explanation.
+                
+                JSON format:
                 {
-                "score": number (0-100),
+                "score": number,
+                "reason": "Explanation here",
                 "missing_keywords": [],
                 "matched_keywords": []
                 }`
@@ -55,10 +69,10 @@ app.post("/send", async (req, res) => {
     const aiResponse = completion.choices[0].message.content;
 
     const cleaned = aiResponse
-    .replace(/```json/g, "")
-    .replace(/```/g, "")
-    .trim();
-    
+        .replace(/```json/g, "")
+        .replace(/```/g, "")
+        .trim();
+
     const parsed = JSON.parse(cleaned);
 
     res.status(200).json({ message: "Working Good", response: parsed });
